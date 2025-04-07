7c8868ac94c1c560f4be4b37454bc6b135903e1c
Helene Arnesen <helene.arnesen@nav.no>
2025-04-07 12:01:40 +0200

    chore: read topic from start

--- src/main/kotlin/no/nav/journey/sykmelding/kafka/SykmeldingListener.kt
+++ src/main/kotlin/no/nav/journey/sykmelding/kafka/SykmeldingListener.kt
@@ -17,7 +17,7 @@ class SykmeldingListener(
 
     @KafkaListener(
         topics = ["\${spring.kafka.topics.sykmeldinger-output}"],
-        groupId = "journey-consumer",
+        groupId = "journey-consumer-1",
         containerFactory = "containerFactory",
     )
     fun listen(cr: ConsumerRecord<String, SykmeldingRecord>) {
