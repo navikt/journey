06e82629516156c352d0ee91a9f1bb1a6e72cc38
Helene Arnesen <helene.arnesen@nav.no>
2025-05-06 09:29:38 +0200

    chore: change topic to tsm.sykmeldinger

--- src/main/kotlin/no/nav/journey/config/pdfgenConfig/PdfGenConfig.kt
+++ src/main/kotlin/no/nav/journey/config/pdfgenConfig/PdfGenConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.journey.config.pdfgenConfig
 
 import jakarta.annotation.PostConstruct
+import no.nav.journey.utils.applog
 import no.nav.pdfgen.core.Environment
 import no.nav.pdfgen.core.PDFGenCore
 import org.springframework.context.annotation.Configuration
@@ -9,11 +10,20 @@ import org.verapdf.gf.foundry.VeraGreenfieldFoundryProvider
 
 @Configuration
 class PdfGenConfig {
-
+    val log = applog()
     @PostConstruct
     fun init() {
-        VeraGreenfieldFoundryProvider.initialise()
-        val coreEnvironment = Environment()
-        PDFGenCore.init(coreEnvironment)
+        try {
+            log.info("Initializing VeraGreenfieldFoundryProvider")
+            VeraGreenfieldFoundryProvider.initialise()
+            log.info("VeraGreenfieldFoundryProvider initialized succesfully")
+            log.info("Initlializing PDFGenCore with coreEnvironment")
+            val coreEnvironment = Environment()
+            PDFGenCore.init(coreEnvironment)
+            log.info("PdfGenCore initialized successfully")
+        } catch (e: Exception) {
+            log.error("Failed to initialize PdfGenConfig: ${e.message}", e)
+            throw RuntimeException("Failed to initialize PdfGenConfig: ${e.message}", e)
+        }
     }
 }
