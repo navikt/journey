bffd1713113f158591a7e075cad6dd44818fa89b
Andreas Sagen Aspaas <danduras@gmail.com>
2025-05-23 10:20:56 +0200

    chore: fix deserializer
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>

--- src/main/kotlin/no/nav/journey/sykmelding/services/util/ObjectMapper.kt
+++ src/main/kotlin/no/nav/journey/sykmelding/services/util/ObjectMapper.kt
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import no.nav.journey.sykmelding.models.SykmeldingModule
 
 val objectMapper: ObjectMapper =
     ObjectMapper().apply {
         registerKotlinModule()
+        registerModule(SykmeldingModule())
         registerModule(JavaTimeModule())
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
