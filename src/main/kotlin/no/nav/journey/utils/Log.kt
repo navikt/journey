8873bf24493f8de12c909242bea7e5ea989ee868
Helene Arnesen <helene.arnesen@nav.no>
2025-05-27 15:49:16 +0200

    fix: log

--- src/main/kotlin/no/nav/journey/utils/Log.kt
+++ src/main/kotlin/no/nav/journey/utils/Log.kt
@@ -1,7 +1,12 @@
 package no.nav.journey.utils
 
+import jakarta.annotation.PostConstruct
+import no.nav.journey.sykmelding.models.Sykmelding
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.bridge.SLF4JBridgeHandler
+import org.springframework.context.annotation.Configuration
+import java.util.logging.LogManager
 
 inline fun <reified T> T.applog(): Logger {
     return LoggerFactory.getLogger(T::class.java)
@@ -10,5 +15,13 @@ inline fun <reified T> T.securelog(): Logger {
     return LoggerFactory.getLogger("securelog")
 }
 
+@Configuration
+class LoggingBridgeConfig {
 
-
+    @PostConstruct
+    fun redirectJulToSlf4j() {
+        LogManager.getLogManager().reset()
+        SLF4JBridgeHandler.removeHandlersForRootLogger()
+        SLF4JBridgeHandler.install()
+    }
+}
