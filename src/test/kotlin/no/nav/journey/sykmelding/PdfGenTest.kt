3f2fb783bd60fd668ed342da64346fd1d7c2e23b
Andreas Sagen Aspaas <danduras@gmail.com>
2025-06-06 11:32:45 +0200

    fix: test

--- src/test/kotlin/no/nav/journey/sykmelding/PdfGenTest.kt
+++ src/test/kotlin/no/nav/journey/sykmelding/PdfGenTest.kt
@@ -2,6 +2,7 @@ package no.nav.journey.sykmelding
 
 import no.nav.journey.sykmelding.models.FlereArbeidsgivere
 import no.nav.journey.pdf.PdfService
+import no.nav.journey.sykmelding.models.XmlSykmelding
 import no.nav.journey.testUtils.extractTextFromPdf
 import no.nav.journey.testUtils.sykmeldingRecord
 import no.nav.pdfgen.core.Environment
@@ -30,19 +31,25 @@ class PdfGenTest {
     fun `generate pdf for sykmelding flere arbeidsgivere`() {
         val recordMedFlereArbeidsgivere = sykmeldingRecord {
             arbeidsgiver = FlereArbeidsgivere("Coop", "Butikkmedarbeider", 80, null, null)
+            sykmelding = (sykmelding as XmlSykmelding).copy(arbeidsgiver = arbeidsgiver)
         }
         val pdfBytes = pdfService.createPdf(recordMedFlereArbeidsgivere)!!
 
         val fil = File("build/test.pdf")
         fil.writeBytes(pdfBytes)
         val tekst = extractTextFromPdf(fil)
-
+        assert(tekst.contains("Flere arbeidsgivere")) { "Mangler 'Flere arbeidsgivere'" }
+        assert(tekst.contains("Coop")) { "Mangler arbeidsgivernavn 'Coop'" }
+        assert(tekst.contains("Butikkmedarbeider")) { "Mangler stillingstittel" }
+        assert(tekst.contains("80")) { "Mangler stillingsprosent '80'" }
     }
 
     @Test
     @Ignore
     fun `generate pdf for sykmelding med hoveddiagnose og bidiagnoser`() {
-        val record = sykmeldingRecord {}
+        val record = sykmeldingRecord {
+            arbeidsgiver = FlereArbeidsgivere("Coop", "Butikkmedarbeider", 80, null, null)
+        }
         val pdfBytes = pdfService.createPdf(record)!!
 
         val fil = File("build/test.pdf")
